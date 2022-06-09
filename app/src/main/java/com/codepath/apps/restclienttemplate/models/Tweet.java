7b5c5fc296c20da5247ad2526f42f715a69cf54e
Gauri Barar <gbarar@fb.com>
2022-06-09 10:27:33 -0700

    added character counter

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -0,0 +1,45 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.parceler.Parcel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Parcel
+public class Tweet {
+
+    public String body;
+    public String createdAt;
+    public User user;
+
+    //empty constructor needed by the parceler library
+    public Tweet() {
+    }
+
+    //turn json to java tweet object
+    public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
+        Tweet tweet = new Tweet();
+        tweet.body = jsonObject.getString("text");
+        tweet.createdAt = jsonObject.getString("created_at");
+        tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
+
+        return tweet;
+
+    }
+
+    //pass in a json array and get a list of tweets
+    public static List<Tweet> fromJsonArray(JSONArray jsonArray) throws JSONException {
+        List<Tweet> tweets = new ArrayList<>();
+
+        //loop thru the json array
+        for (int i = 0; i < jsonArray.length(); i++) {
+            tweets.add(fromJson(jsonArray.getJSONObject(i)));
+        }
+
+        return tweets;
+    }
+
+}
