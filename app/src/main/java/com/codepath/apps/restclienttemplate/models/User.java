67f1a9e576df8c1d4353e925582d50141c3f04ef
Gauri Barar <gbarar@fb.com>
2022-06-10 15:21:31 -0700

    updated the UI

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -1,5 +1,7 @@
 package com.codepath.apps.restclienttemplate.models;
 
+import android.util.Log;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.parceler.Parcel;
@@ -10,6 +12,7 @@ public class User {
     public String name;
     public String screenName;
     public String profileImageUrl;
+    public static final String TAG = "";
 
     //empty constructor needed by the parceler library
     public User() {
@@ -20,7 +23,7 @@ public class User {
         user.name = jsonObject.getString("name");
         user.screenName = jsonObject.getString("screen_name");
         user.profileImageUrl = jsonObject.getString("profile_image_url_https");
-
+        Log.d(TAG, "profile image url" + jsonObject.getString("profile_image_url_https"));
 
         return user;
 
